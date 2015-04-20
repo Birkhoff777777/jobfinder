5bbd24d3b62f7e7471c1394d6d99e3e44d60cde4
杨鹏 <yangpeng@yangpengdeMacBook-Pro.local>
2015-04-20 16:09:31 -0400

    Initial Commit

---
+++ JobFinderTests/JobFinderTests.swift
@@ -0,0 +1,36 @@
+//
+//  JobFinderTests.swift
+//  JobFinderTests
+//
+//  Created by 杨鹏 on 4/20/15.
+//  Copyright (c) 2015 pengyang. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class JobFinderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
